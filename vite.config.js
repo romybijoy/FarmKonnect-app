5404c01e08616ab061c24cead1130da94c6da98f
romy <romyjimmy17@gmail.com>
2025-05-31 07:08:37 +0530

    tailwind css added

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
+import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  plugins: [react()],
-  esbuild: {
-    loader: 'jsx', // Ensures JSX support
-  },
-  server: {
-    port: 5173,
-  },
+  plugins: [
+    tailwindcss(),
+  ],
 })
