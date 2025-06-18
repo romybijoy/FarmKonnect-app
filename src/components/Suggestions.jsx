289cfb81c8660408d49a3bcb70fb6aad4fcee5d3
romy <romyjimmy17@gmail.com>
2025-06-18 18:29:56 +0530

    follow - missed file

--- src/components/Suggestions.jsx
+++ src/components/Suggestions.jsx
@@ -3,34 +3,40 @@ import prof1 from '../assets/prof1.jpeg';
 import prof2 from '../assets/prof4.jpeg';
 import prof3 from '../assets/prof2.jpeg';
 import prof4 from '../assets/prof3.jpeg';
+import FollowButton from './follow/FollowButton';
 
 function Suggestions() {
-  const [suggestion, setSuggestion] = useState([
-    {
-      id: 1,
-      user: 'John Doe',
-      img: prof1,
-      status: 'Follows you',
-    },
-    {
-      id: 2,
-      user: 'Alice Bow',
-      img: prof2,
-      status: 'Followed by Regha',
-    },
-    {
-      id: 3,
-      user: 'Manoj',
-      img: prof3,
-      status: 'Follows you',
-    },
-    {
-      id: 4,
-      user: 'Albin Joe',
-      img: prof4,
-      status: 'Follows you',
-    },
-  ]);
+  const users = JSON.parse(localStorage.getItem("userList")) || [];
+  const me = JSON.parse(localStorage.getItem("myInfo")) || [];
+  const [suggestion, setSuggestion] = useState(users);
+    
+  //   [
+  //   {
+  //     id: 1,
+  //     user: 'John Doe',
+  //     img: prof1,
+  //     status: 'Follows you',
+  //   },
+  //   {
+  //     id: 2,
+  //     user: 'Alice Bow',
+  //     img: prof2,
+  //     status: 'Followed by Regha',
+  //   },
+  //   {
+  //     id: 3,
+  //     user: 'Manoj',
+  //     img: prof3,
+  //     status: 'Follows you',
+  //   },
+  //   {
+  //     id: 4,
+  //     user: 'Albin Joe',
+  //     img: prof4,
+  //     status: 'Follows you',
+  //   },
+  // ]
+  // );
 
   return (
     <div className="w-full max-w-md mx-auto p-4 bg-white shadow rounded-xl mt-5">
@@ -44,16 +50,17 @@ function Suggestions() {
             <div key={item.id} className="flex items-center justify-between">
               <div className="flex items-center gap-3">
                 <img
-                  src={item.img}
+                  src={item.image}
                   alt="Profile"
                   className="w-10 h-10 rounded-full object-cover border"
                 />
                 <div>
-                  <p className="text-sm font-medium text-gray-800">{item.user}</p>
-                  <p className="text-xs text-gray-500">{item.status}</p>
+                  <p className="text-sm font-medium text-gray-800">{item.name}</p>
+                  {/* <p className="text-xs text-gray-500">{item.status}</p> */}
                 </div>
               </div>
-              <button className="text-sm text-blue-500 font-semibold hover:underline">Connect</button>
+              {/* <button className="text-sm text-blue-500 font-semibold hover:underline">Connect</button> */}
+              <FollowButton viewerId={me.id} targetUserId={item.id}/>
             </div>
           ))}
         </div>
